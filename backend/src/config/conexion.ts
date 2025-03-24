a1728f760b48e882df5cc145698bdcf0c60f5a90
Angie <navarroangarita123@gmail.com>
2025-03-24 14:02:09 -0500

    ajustes para vercel

--- backend/src/config/conexion.ts
+++ backend/src/config/conexion.ts
@@ -1,9 +1,19 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('inventario', 'root', 'Angie2024Mysql', {
-    host: 'localhost',
-    dialect: 'mysql'
+// const sequelize = new Sequelize('inventario', 'root', 'Angie2024Mysql', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+// export default sequelize;
+import { Sequelize } from "sequelize";
+import mysql2 from "mysql2"; // <-- Asegurar que está importado
+
+const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
+  host: process.env.DB_HOST!,
+  dialect: "mysql",
+  dialectModule: mysql2, // <-- Agregar esto explícitamente
+  logging: false,
 });
 
 export default sequelize;
